c0fc7f5e0608e072be54e5933bbceae512e4a72d
Hassanalmaymn <144214579+Hassanalmaymn@users.noreply.github.com>
2025-08-04 15:50:53 +0300

    fix the service so we added time and data

--- app/src/main/java/com/example/app/service/DataBaseReportGeneratorService.java
+++ app/src/main/java/com/example/app/service/DataBaseReportGeneratorService.java
@@ -34,16 +34,17 @@ public class DataBaseReportGeneratorService {
                 InputStream reportStream = resource.getInputStream();
                 Connection conn = DataSourceUtils.getConnection(dataSource)
         ) {
-            // Compile the JRXML
+
             JasperReport jasperReport = JasperCompileManager.compileReport(reportStream);
 
-            // Empty parameters map (or add your own params)
+
             Map<String, Object> parameters = new HashMap<>();
 
-            // Fill the report
+
+
             JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, conn);
 
-            // Export to PDF
+
             return JasperExportManager.exportReportToPdf(jasperPrint);
         } catch (Exception e) {
             throw new RuntimeException("Error generating Jasper report: " + e.getMessage(), e);
